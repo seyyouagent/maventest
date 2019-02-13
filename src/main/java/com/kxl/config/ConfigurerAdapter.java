95b4651e44797b3fb05e1e01504593ae9523da3a
liuxiaoyu <liuxiaoyu@kingxunlian.com>
2019-02-13 15:45:18 +0800

    调整结构

--- src/main/java/com/kxl/config/ConfigurerAdapter.java
+++ src/main/java/com/kxl/config/ConfigurerAdapter.java
@@ -1,5 +1,6 @@
-package com.kxl.filter;
+package com.kxl.config;
 
+import com.kxl.filter.AuthInterceptorAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
--- src/main/java/com/kxl/service/UserService.java
+++ src/main/java/com/kxl/service/UserService.java
@@ -1,14 +1,11 @@
 package com.kxl.service;
 
 import com.kxl.Dto.UserDto;
-import com.kxl.bo.RoleBo;
 import com.kxl.bo.UserBo;
-import com.kxl.mapper.RoleMapper;
 import com.kxl.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
